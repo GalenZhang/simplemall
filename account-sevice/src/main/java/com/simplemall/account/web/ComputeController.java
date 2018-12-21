1deb96e7deca705aec764d0372c7e5a1db2864a0
galen.zhang <z60014840@163.com>
2018-12-21 23:03:32 +0800

    修复依赖版本冲突问题

--- account-sevice/src/main/java/com/simplemall/account/web/ComputeController.java
+++ account-sevice/src/main/java/com/simplemall/account/web/ComputeController.java
@@ -1,5 +1,6 @@
 package com.simplemall.account.web;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +30,9 @@ public class ComputeController {
 
     @Autowired
     private DiscoveryClient client;
+    
+    @Autowired
+    private Registration registration; // 服务注册
     
 //    static{
 //        Timer timer = new Timer();
@@ -77,7 +82,8 @@ public class ComputeController {
 	        return "From Service-B, Result is " + r+"\nPort:"+instance.getPort();
         }*/
         
-        ServiceInstance instance = client.getLocalServiceInstance();
+    	List<ServiceInstance> list = client.getInstances(registration.getServiceId());
+        ServiceInstance instance = list.get(0);//client.getLocalServiceInstance();
         Integer r = a + b;
         logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return "From Service-B, Result is " + r+"\nPort:"+instance.getPort();
--- base-service/admin-server/src/main/java/com/simplemall/micro/serv/base/admin/AdminServerApplicaton.java
+++ base-service/admin-server/src/main/java/com/simplemall/micro/serv/base/admin/AdminServerApplicaton.java
@@ -3,14 +3,12 @@ package com.simplemall.micro.serv.base.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 
 @SpringBootApplication
 @EnableAdminServer
-@Configuration
 @EnableDiscoveryClient
 public class AdminServerApplicaton {
 
